04e3de9013c40ac50face541fb2da9173ebcec94
richard friedman <friedman.richard@gmail.com>
2012-09-16 22:01:11 -0400

    second commit

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using EbayFactory;
 
 namespace EbayServiceHost
 {
@@ -11,6 +12,8 @@ namespace EbayServiceHost
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+      
+
             Response.Redirect("~/EbayService.svc");
         }
     }
